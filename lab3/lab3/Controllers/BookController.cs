288e607bd6a24383da13b4615b541d9f221b1ebd
nkozkien <nkozkien@gmail.com>
2021-06-24 10:59:40 +0700

    Initial commit

---
+++ lab3/lab3/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using lab3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab3.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult ListBook()
+        {
+            BookManagerContext context = new BookManagerContext();
+            var listBook = context.Books.ToList();
+            return View(listBook);
+        }
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        public ActionResult CreateBook()
+        {
+            Book book = new Book();
+            BookManagerContext context = new BookManagerContext();
+            context.Books.Add(book);
+            context.SaveChanges();
+            return View(book);
+        }
+    }
+
+}
